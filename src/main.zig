9c73c7e41db0740a9e27b34b641f466b11795640
nikneym <halildrk@gmail.com>
2023-02-28 15:09:24 +0300

    leaky image draw

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const glfw = @import("glfw");
 const gl = @import("zgl");
+const zstbi = @import("zstbi");
 
 fn glLoader(_: @TypeOf(.{}), name: [:0]const u8) ?*const anyopaque {
     return glfw.getProcAddress(name);
@@ -17,6 +18,10 @@ pub fn main() !void {
     _ = glfw.init(.{});
     defer glfw.terminate();
 
+    zstbi.init(allocator);
+    defer zstbi.deinit();
+
+    // FIXME: do not handle nullables like this
     const window = glfw.Window.create(640, 480, "OpenGL basics", null, null, .{
         .context_version_major = 3,
         .context_version_minor = 3,
@@ -73,19 +78,47 @@ pub fn main() !void {
     defer vbo.delete();
     vbo.bind(.array_buffer);
     vbo.data(f32, &[_]f32{
-        // vertices     // colors
-        0.0, 1.0, 0.0,  1.0, 0.0, 0.0,
-        -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
-        0.5, 0.0, 0.0,  0.0, 0.0, 1.0,
+    // positions       // colors        // texture coords
+     0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0,   // top right
+     0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0,   // bottom right
+    -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0,   // bottom left
+    -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0    // top left 
+    }, .static_draw);
+
+    var ebo = gl.Buffer.gen();
+    defer ebo.delete();
+    ebo.bind(.array_buffer);
+    ebo.data(u8, &[_]u8{
+        0, 1, 3,
+        1, 2, 3,
     }, .static_draw);
 
     // position attribute
-    gl.vertexAttribPointer(0, 3, .float, false, 6 * @sizeOf(f32), 0);
+    gl.vertexAttribPointer(0, 3, .float, false, 8 * @sizeOf(f32), 0);
     vao.enableVertexAttribute(0);
 
     // color attribute
-    gl.vertexAttribPointer(1, 3, .float, false, 6 * @sizeOf(f32), 3 * @sizeOf(f32));
+    gl.vertexAttribPointer(1, 3, .float, false, 8 * @sizeOf(f32), 3 * @sizeOf(f32));
     vao.enableVertexAttribute(1);
+
+    // tex coord attribute
+    gl.vertexAttribPointer(2, 2, .float, false, 8 * @sizeOf(f32), 6 * @sizeOf(f32));
+    vao.enableVertexAttribute(2);
+
+    var texture = gl.genTexture();
+    gl.bindTexture(texture, .@"2d");
+
+    // set wrapping parameters
+    gl.texParameter(.@"2d", .wrap_s, .mirrored_repeat);
+    gl.texParameter(.@"2d", .wrap_t, .mirrored_repeat);
+    // set filtering parameters
+    gl.texParameter(.@"2d", .min_filter, .linear_mipmap_linear);
+    gl.texParameter(.@"2d", .mag_filter, .linear);
+
+    var image = try zstbi.Image.loadFromFile("asset/wall.jpg", 3);
+    defer image.deinit();
+    gl.textureImage2D(.@"2d", 0, .rgb, image.width, image.height, .rgb, .unsigned_byte, @ptrCast([*]const u8, image.data));
+    gl.generateMipmap(.@"2d");
 
     while (!window.shouldClose()) {
         // render here
@@ -96,10 +129,12 @@ pub fn main() !void {
             .stencil = true,
         });
 
-        shaderProgram.use();
+        texture.bind(.@"2d");
 
+        shaderProgram.use();
         vao.bind();
-        gl.drawArrays(.triangles, 0, 3);
+
+        gl.drawElements(.triangles, 6, .u8, 0);
 
         // swap front end back buffers
         window.swapBuffers();
--- src/vertex.glsl
+++ src/vertex.glsl
@@ -1,10 +1,13 @@
 #version 330 core
 layout (location = 0) in vec3 aPos; // position variable has attribute position 0
 layout (location = 1) in vec3 aColor; // color variable has attribute position 1
+layout (location = 2) in vec2 aTexCoord; // tex coord variable has attribute position 2
 
 out vec3 ourColor;
+out vec2 TexCoord;
 
 void main() {
-   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+   gl_Position = vec4(aPos, 1.0);
    ourColor = aColor;
+   TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 };
